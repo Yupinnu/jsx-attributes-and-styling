59db4fbef074f42b7666edb39a9ce60692d5202b
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2021-05-19 21:38:33 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img
+        className="dog-img"
+        src="https://i.ytimg.com/vi/hPsHAABCbRk/maxresdefault.jpg"
+      />
+      <img
+        className="dog-img"
+        src="https://i.ytimg.com/vi/EVqxcWQOEnU/maxresdefault.jpg"
+      />
+      <img
+        className="dog-img"
+        src="https://www.rd.com/wp-content/uploads/2017/10/These-Funny-Dog-Videos-Are-the-Break-You-Need-Right-Now_493370860-Jenn_C.jpg?resize=640,426"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
